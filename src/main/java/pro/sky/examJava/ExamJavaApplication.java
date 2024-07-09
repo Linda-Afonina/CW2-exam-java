98f54d3dd881de00cd96ccf154a9d46f8ff25a23
linda <charming.linda85@gmail.com>
2024-07-09 09:59:13 +0300

    Initial commit

---
+++ src/main/java/pro/sky/examJava/ExamJavaApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.examJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/examJava/ExamJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.examJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
